7cfa7312dc636564959bcb54988dd07a41c80ba7
Ronaldo Brasil <marcosronaldo.qa@gmail.com>
2025-02-03 18:14:15 -0300

    :test_tube: test: Automatizando teste de Cadastro

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,14 +1,48 @@
-describe('Cadastro', () => {
-    it('', () => {
+import { Access } from "../support/actions/access"
 
-      
+describe('Dado que estou na página de cadastro', () => {
 
-      cy.visit('https://front.serverest.dev/login') 
-      cy.get('a[data-testid="cadastrar"]').click()
-      cy.get('input[data-testid="nome"]').type('Ronaldo Brasil')
-      cy.get('input[data-testid="email"]').type('emailronaldoteste@gmail.com')
-      cy.get('input[data-testid="password"]').type('strongpassword12344321')
-      
+  context('Quando preencho os campos com dados de forma correta sem selecionar a caixa de administrador', function(){
 
+  
+    it('Então de deve ser possível criar uma conta como usuário ', () => {
+
+      const user = {
+        name: 'Ronaldo Brasil',
+        email: 'emailronaldoteste@gmail.com',
+        password: 'strongpassword12344321',
+        adm: 'false'
+      }
+
+      cy.deleteUserByEmail(user.email)
+      Access.goC()
+      Access.fillformC(user)
+      Access.submitC()
+      Access.userShouldSignup('Serverest Store')
     })
+    
   })
+
+  context('Quando preencho os campos com dados de forma correta e seleciono a caixa de administrador', function(){
+
+  
+    it('Então de deve ser possível criar uma conta como Administrador ', () => {
+
+      const user = {
+        name: 'Ronaldo Brasil ADM',
+        email: 'emailronaldoADM@gmail.com',
+        password: 'strongpassword12344321',
+        adm: 'True'
+      }
+
+      cy.deleteUserByEmail(user.email)
+      Access.goC()
+      Access.fillformC(user)
+      Access.Adm()
+      Access.submitC()
+      Access.userShouldSignup(`Bem Vindo  ${user.name}`)
+    })
+ 
+  })
+
+})
