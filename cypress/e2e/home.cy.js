36808d13dd1524453a2d81c5b6c65ef1b95b177f
Ronaldo Brasil <marcosronaldo.qa@gmail.com>
2025-02-14 16:22:59 -0300

    :test_tube: test: Criando testes da Home

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,4 +1,7 @@
 import { Home } from "../support/actions/Home"
+import { ProductList } from "../support/actions/productList"
+import { ProductDetails } from "../support/actions/productDetails"
+import { Header } from "../support/actions/components/header"
 
 describe('Dado que estou na Página de Inicial', function () {
 
@@ -9,27 +12,152 @@ describe('Dado que estou na Página de Inicial', function () {
 
     cy.fixture('products/tech').then(function (tech) {
       this.tech = tech
-    })
+    }) 
   })
 
   context('Quando click no botão a Lista', function () {
 
-    it.only('Então o produto deve ir para a Lista de Compras', function () {
+    it('Então o produto deve ir para a Lista de Compras', function () {
 
       const user = this.successful.user
       const product = this.tech.laptop
       const admin = this.successful.admin
 
+      cy.adjustUserData(admin)
+      cy.adjustUserData(user)
+
       cy.deleteProductByName(admin, product.nome)
-      cy.postProduct(admin,product)
+      cy.postProduct(admin, product)
 
       Home.go(user)
       Home.addProductToList(product)
+      ProductList.shouldHaveProduct(product)
 
 
     })
 
   })
+
+  context('Quando click no botão Detalhes', function () {
+
+    it('Então o devo ser redirecionado para página Detalhes do Produto', function () {
+
+      const user = this.successful.user
+      const product = this.tech.monitor
+      const admin = this.successful.admin
+
+      cy.adjustUserData(admin)
+      cy.adjustUserData(user)
+      cy.deleteProductByName(admin, product.nome)
+      cy.postProduct(admin, product)
+
+      Home.go(user)
+      Home.goToProductDetails(product)
+      ProductDetails.verifyDetails(product)
+
+
+    })
+
+  })
+
+  context('Quando click no botão de Carrinho', function () {
+
+    it('Então o devo ser redirecionado para página de Lista de Compras', function () {
+
+      const user = this.successful.user
+
+      cy.adjustUserData(user)
+
+      Home.go(user)
+      Home.clickOnCart()
+      ProductList.shouldBeVisible()
+
+    })
+
+  })
+
+  context('Quando pesquiso pelo produto', function () {
+
+    it('Então o sistema deve me retornar o filtro corretamente', function () {
+
+      const user = this.successful.user
+      const product = this.tech.teclado
+      const admin = this.successful.admin
+
+      cy.adjustUserData(admin)
+      cy.adjustUserData(user)
+
+      cy.deleteProductByName(admin, product.nome)
+      cy.postProduct(admin, product)
+
+      Home.go(user)
+      Home.searchProduct(product.nome)
+      Home.shouldHaveProduct(product)
+
+    })
+
+    it('Então o sistema não deve me retornar nenhum produto', function () {
+
+      const user = this.successful.user
+
+      cy.adjustUserData(user)
+
+      Home.go(user)
+      Home.searchProduct('Produto Ruim')
+      Home.notFoundShouldHaveTxt('Nenhum produto foi encontrado')
+    })
+
+  })
+
+  context('Quando click nos botões do header', function () {
+
+    it('Então o devo ser redirecionado para a página de Lista de Compras', function () {
+
+      const user = this.successful.user
+
+      cy.adjustUserData(user)
+
+      Home.go(user)
+      Header.goToProductList
+
+    })
+
+    it('Então o devo ser redirecionado para a página de carrinho', function () {
+
+      const user = this.successful.user
+
+      cy.adjustUserData(user)
+
+      Home.go(user)
+      Header.goToCart() 
+    
+    })
+
+    it('Então o devo ser redirecionado para a página de carrinho', function () {
+
+      const user = this.successful.user
+
+      cy.adjustUserData(user)
+
+      Home.go(user)
+      Header.goToHome()
+    
+    })
+
+    it('Então o devo ser possível fazer Logout', function () {
+
+      const user = this.successful.user
+
+      cy.adjustUserData(user)
+
+      Home.go(user)
+      Header.logout()
+      
+    
+    })
+
+  })
+
 })
 
 
