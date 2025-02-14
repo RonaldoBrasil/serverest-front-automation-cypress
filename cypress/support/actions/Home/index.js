c6934ef5e3144cd5163672fc64642da8086f8e91
Ronaldo Brasil <marcosronaldo.qa@gmail.com>
2025-02-13 22:06:58 -0300

    :card_file_box: Org: Ajustando PGo + Criação de Comandos Customizados

--- cypress/support/actions/Home/index.js
+++ cypress/support/actions/Home/index.js
@@ -5,4 +5,22 @@
         cy.get('section[class="row espacamento"]').should('be.visible')
     },
 
- }
+
+go: function (user) {
+
+           cy.apiLogin(user).then(function(response){
+                window.localStorage.setItem('serverest/userEmail', user.email)
+                window.localStorage.setItem('serverest/userToken', response.body.authorization)
+            })
+
+        cy.visit('/home')
+    },
+
+    addProductToList: function(product){
+        cy.contains('h5', product.nome)
+        .parent('div[class="card-body"]')
+        .find('button').click()
+    },
+
+
+}
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,40 +24,102 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('getUserByEmail',(email) => {
-    cy.api({
-        url: `${Cypress.env('apiUrl')}/usuarios`,
-        method: 'GET',
-      }).then(response => {
-        const users = response.body.usuarios || response.body
-        expect(Array.isArray(users)).to.be.true
-        const user = users.find(u => u.email === email)
-        return user
-      })
+Cypress.Commands.add('getUserByEmail', (email) => {
+  cy.api({
+    url: `${Cypress.env('apiUrl')}/usuarios`,
+    method: 'GET',
+  }).then(response => {
+    const users = response.body.usuarios || response.body
+    expect(Array.isArray(users)).to.be.true
+    const user = users.find(u => u.email === email)
+    return user
+  })
 })
 
-Cypress.Commands.add('deleteUserByEmail',(email) =>{
+Cypress.Commands.add('deleteUserByEmail', (email) => {
+  cy.getUserByEmail(email).then(user => {
+    expect(user).to.not.be.undefined
+    const userId = user._id
 
-    cy.getUserByEmail(email).then(user => {
-        expect(user).to.not.be.undefined
-        const userId = user._id
-
-        cy.api({
-            url: `${Cypress.env('apiUrl')}/usuarios/${userId}`,
-            method: 'DELETE',
-          })
+    cy.api({
+      url: `${Cypress.env('apiUrl')}/usuarios/${userId}`,
+      method: 'DELETE',
     })
+  })
 })
 
-Cypress.Commands.add('postUser',(user) => {
-    cy.api({
-        url: `${Cypress.env('apiUrl')}/usuarios`,
-        method: 'POST',
-        body: {
-          "nome": user.name,
-          "email": user.email,
-          "password": user.password,
-          "administrador": user.adm
-        }
-      })
+Cypress.Commands.add('postUser', (user) => {
+  cy.api({
+    url: `${Cypress.env('apiUrl')}/usuarios`,
+    method: 'POST',
+    body: {
+      "nome": user.name,
+      "email": user.email,
+      "password": user.password,
+      "administrador": user.adm
+    }
+  })
 })
+
+Cypress.Commands.add('apiLogin', (user) => {
+  cy.api({
+    url: `${Cypress.env('apiUrl')}/login`,
+    method: 'POST',
+    body: {
+      "email": user.email,
+      "password": user.password,
+    }
+  })
+
+})
+
+Cypress.Commands.add('postProduct', (user, product) => {
+
+  cy.apiLogin(user).then(function (response) {
+
+    cy.api({
+      url: `${Cypress.env('apiUrl')}/produtos`,
+      method: 'POST',
+      headers: { authorization: response.body.authorization },
+      body: {
+        "nome": product.nome,
+        "preco": product.preco,
+        "descricao": product.descricao,
+        "quantidade": product.quantidade
+      }
+    })
+
+  })
+
+})
+
+Cypress.Commands.add('getProductByName', (name) => {
+  cy.api({
+    url: `${Cypress.env('apiUrl')}/produtos`,
+    method: 'GET',
+  }).then(response => {
+    const products = response.body.produtos || response.body
+    expect(Array.isArray(products)).to.be.true
+    const product = products.find(p => p.nome === name)
+    return product
+  })
+
+})
+
+Cypress.Commands.add('deleteProductByName', (user,name) => {
+  cy.getProductByName(name).then(product => {
+    expect(product).to.not.be.undefined
+    const productId = product._id
+
+    cy.apiLogin(user).then(function(response){
+
+      cy.api({
+        url: `${Cypress.env('apiUrl')}/produtos/${productId}`,
+        headers: { authorization: response.body.authorization },
+        method: 'DELETE',
+      })
+    })
+    
+  })
+})
+
